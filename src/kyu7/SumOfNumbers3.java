347b9b8d98e0366dc7649a5d592dba688a18834b
Vitali Holod <holod.vitali@gmail.com>
2019-03-05 21:00:49 +0300

    Refactoring

--- src/kyu7/SumOfNumbers3.java
+++
@@ -1,8 +0,0 @@
-package kyu7;
-
-public class SumOfNumbers3 {
-    public int GetSum(int a, int b) {
-
-        return (a + b) * (Math.abs(a - b) + 1) / 2;
-    }
-}
--- src/kyu8/EvenOrOdd.java
+++
@@ -1,8 +0,0 @@
-package kyu8;
-
-public class EvenOrOdd {
-    public static String even_or_odd(int number) {
-
-        return number % 2 == 0 ? "Even" : "Odd";
-    }
-}
